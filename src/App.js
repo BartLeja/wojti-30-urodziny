857665b3fc4a188105d9864fd044e4174c966865
bartlomiej.leja <bartlomiej.leja@caspio.com>
2022-07-14 11:44:25 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,553 @@
-import logo from './logo.svg';
 import './App.css';
+import { ProGallery } from 'pro-gallery';
+import 'pro-gallery/dist/statics/main.css';
+import wojti1 from './images/wojti1.jfif';
+import wojti2 from './images/wojti2.jfif';
+import wojti3 from './images/wojti3.jfif';
+import wojti4 from './images/wojti4.jfif';
+import wojti5 from './images/wojti5.jfif';
+import wojti6 from './images/wojti6.jfif';
+import wojti7 from './images/wojti7.jfif';
+import wojti8 from './images/wojti8.jfif';
+import wojti9 from './images/wojti9.jfif';
+import wojti10 from './images/wojti10.jfif';
+import wojti11 from './images/wojti11.jfif';
+import wojti12 from './images/wojti12.jfif';
+import wojti13 from './images/wojti13.jfif';
+import wojti15 from './images/wojti15.jfif';
+import wojti16 from './images/wojti16.jfif';
+import wojti17 from './images/wojti17.jfif';
+import wojti18 from './images/wojti18.jfif';
+import wojti19 from './images/wojti19.jfif';
+import wojti20 from './images/wojti20.jfif';
+import wojti21 from './images/wojti21.jfif';
+import wojti22 from './images/wojti22.jfif';
+import wojti23 from './images/wojti23.jfif';
+import wojti24 from './images/wojti24.jfif';
+import wojti25 from './images/wojti25.jfif';
+import wojti26 from './images/wojti26.jfif';
+import wojti27 from './images/wojti27.jfif';
+import wojti28 from './images/wojti28.jfif';
+import wojti29 from './images/wojti29.jfif';
+import wojti30 from './images/wojti30.jfif';
+import wojti31 from './images/wojti31.jfif';
+import wojti32 from './images/wojti32.jfif';
+import wojti33 from './images/wojti33.jfif';
+import wojti34 from './images/wojti34.jfif';
+import wojti35 from './images/wojti35.jfif';
+import wojti36 from './images/wojti36.jfif';
+import wojti37 from './images/wojti37.jfif';
+import wojti38 from './images/wojti38.jfif';
+import wojti39 from './images/wojti39.jfif';
+import wojti40 from './images/wojti40.jfif';
+import wojti41 from './images/wojti41.jfif';
+import wojti42 from './images/wojti42.jfif';
+import wojti43 from './images/wojti43.jfif';
+import wojti44 from './images/wojti44.jfif';
+import wojti45 from './images/wojti45.jfif';
+import wojti46 from './images/wojti46.jfif';
+import wojti47 from './images/wojti47.jfif';
+import wojti48 from './images/wojti48.jfif';
+import wojti49 from './images/wojti49.jfif';
+import wojti50 from './images/wojti50.jfif';
+import wojti51 from './images/wojti51.jfif';
+import wojti52 from './images/wojti52.jfif';
+import wojti53 from './images/wojti53.jfif';
+import wojti54 from './images/wojti54.jfif';
+import wojti55 from './images/wojti55.jfif';
+import wojti56 from './images/wojti56.jfif';
+import wojti57 from './images/wojti57.jfif';
+import wojti58 from './images/wojti58.jfif';
+import wojti59 from './images/wojti59.jfif';
+import wojti60 from './images/wojti60.jfif';
+import wojti61 from './images/wojti61.jfif';
+import wojti62 from './images/wojti62.jfif';
+import wojti63 from './images/wojti63.jfif';
+import wojti64 from './images/wojti64.jfif';
+import wojti65 from './images/wojti65.jfif';
+import wojti66 from './images/wojti66.jfif';
+import wojti67 from './images/wojti67.jfif';
+import wojti68 from './images/wojti68.jfif';
+import wojti69 from './images/wojti69.jfif';
+import wojti70 from './images/wojti70.jfif';
+import wojti71 from './images/wojti71.jfif';
+import wojti72 from './images/wojti72.jfif';
+import wojti73 from './images/wojti73.jfif';
+import wojti74 from './images/wojti74.jfif';
+import wojti75 from './images/wojti75.jfif';
+import wojti76 from './images/wojti76.jfif';
+import wojti77 from './images/wojti77.jfif';
+import wojti78 from './images/wojti78.jfif';
+import wojti79 from './images/wojti79.jfif';
+import wojti80 from './images/wojti80.jfif';
+import wojti81 from './images/wojti81.jfif';
+import wojti82 from './images/wojti82.jfif';
+import wojti83 from './images/wojti83.jfif';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let itemsArray = [
+            {
+                mediaUrl: wojti1,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti2,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti3,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti4,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti5,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti6,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti7,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti8,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti9,
+                height: 200,
+                width: 250,
+            },
+            {
+                mediaUrl: wojti10,
+                height: 200,
+                width: 250,
+            },
+            {
+                mediaUrl: wojti11,
+                height: 200,
+                width: 270,
+            },
+            {
+                mediaUrl: wojti12,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti13,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti15,
+                height: 200,
+                width: 220,
+            },
+            {
+                mediaUrl: wojti16,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti17,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti18,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti19,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti20,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti21,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti22,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti23,
+                height: 200,
+                width: 150,
+            },
+            {
+                mediaUrl: wojti24,
+                height: 200,
+                width: 280,
+            },
+            {
+                mediaUrl: wojti25,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti26,
+                height: 200,
+                width: 180,
+            },
+            {
+                mediaUrl: wojti27,
+                height: 200,
+                width: 150,
+            },
+            {
+                mediaUrl: wojti28,
+                height: 200,
+                width: 180,
+            },
+            {
+                mediaUrl: wojti29,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti30,
+                height: 200,
+                width: 220,
+            },
+            {
+                mediaUrl: wojti31,
+                height: 200,
+                width: 220,
+            },
+            {
+                mediaUrl: wojti32,
+                height: 200,
+                width: 160,
+            },
+            {
+                mediaUrl: wojti33,
+                height: 200,
+                width: 300,
+            },
+            {
+                mediaUrl: wojti34,
+                height: 200,
+                width: 220,
+            },
+            {
+                mediaUrl: wojti35,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti36,
+                height: 200,
+                width: 150,
+            },
+            {
+                mediaUrl: wojti37,
+                height: 200,
+                width: 220,
+            },
+            {
+                mediaUrl: wojti38,
+                height: 200,
+                width: 220,
+            },
+            {
+                mediaUrl: wojti39,
+                height: 200,
+                width: 150,
+            },
+            {
+                mediaUrl: wojti40,
+                height: 200,
+                width: 150,
+            },
+            {
+                mediaUrl: wojti41,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti42,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti43,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti44,
+                height: 200,
+                width: 140,
+            },
+            {
+                mediaUrl: wojti45,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti46,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti47,
+                height: 200,
+                width: 180,
+            },
+            {
+                mediaUrl: wojti48,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti49,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti50,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti51,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti52,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti53,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti54,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti55,
+                height: 200,
+                width: 220,
+            },
+            {
+                mediaUrl: wojti56,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti57,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti58,
+                height: 200,
+                width: 140,
+            },
+            {
+                mediaUrl: wojti59,
+                height: 200,
+                width: 220,
+            },
+            {
+                mediaUrl: wojti60,
+                height: 200,
+                width: 220,
+            },
+            {
+                mediaUrl: wojti61,
+                height: 200,
+                width: 150,
+            },
+            {
+                mediaUrl: wojti62,
+                height: 200,
+                width: 240,
+            },
+            {
+                mediaUrl: wojti63,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti64,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti65,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti66,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti67,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti68,
+                height: 200,
+                width: 280,
+            },
+            {
+                mediaUrl: wojti69,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti70,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti71,
+                height: 200,
+                width: 150,
+            },
+            {
+                mediaUrl: wojti72,
+                height: 200,
+                width: 150,
+            },
+            {
+                mediaUrl: wojti73,
+                height: 200,
+                width: 230,
+            },
+            {
+                mediaUrl: wojti74,
+                height: 200,
+                width: 250,
+            },
+            {
+                mediaUrl: wojti75,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti76,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti77,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti78,
+                height: 200,
+                width: 200,
+            },
+            {
+                mediaUrl: wojti79,
+                height: 200,
+                width: 100,
+            },
+            {
+                mediaUrl: wojti80,
+                height: 200,
+                width: 130,
+            },
+            {
+                mediaUrl: wojti81,
+                height: 200,
+                width: 180,
+            },
+            {
+                mediaUrl: wojti82,
+                height: 200,
+                width: 150,
+            },
+            {
+                mediaUrl: wojti83,
+                height: 200,
+                width: 200,
+            },
+            
+
+    ]
+    let items = [];
+    itemsArray.forEach(element => {
+        items.push({
+                itemId: 'differentItem',
+                mediaUrl: element.mediaUrl,
+                metaData: {
+                        type: 'image',
+                        height:  element.height,
+                        width: element.width,
+                        title: 'Urodzinowe foty wojtiego',
+                        description: 'sample-description',
+                        focalPoint: [0, 0],
+                        link: {
+                                url: 'http://example.com',
+                                target: '_blank'
+                        },
+                } 
+        })  
+    });
+
+    // The options of the gallery (from the playground current state)
+    const options = {
+        galleryLayout: 0,
+        imageHoverAnimation: 'ZOOM_IN'
+    };
+
+    // The size of the gallery container. The images will fit themselves in it
+    const container = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+
+    // The eventsListener will notify you anytime something has happened in the gallery.
+    const eventsListener = (eventName, eventData) => console.log({eventName, eventData}); 
+
+    // The scrollingElement is usually the window, if you are scrolling inside another element, suplly it here
+    const scrollingElement = window;
+
+    return (
+        <>
+                <div className='generated-text'>Wszytskiego Najlepszego z Okazji 30-stych Urodzin BYKU !!!!</div>
+                <ProGallery
+                        items={items}
+                        options={options}
+                        container={container}
+                        scrollingElement={scrollingElement}
+                />
+        </>
+    );
 }
 
 export default App;
